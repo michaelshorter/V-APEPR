b76b0c24463d2d7b6c178f84ac57a4370c41ab61
michaelshorter <mail@mrshorter.co.uk>
2023-05-23 09:38:50 +0100

    Update CostCounter.py

--- CostCounter.py
+++ CostCounter.py
@@ -7,8 +7,8 @@ def Cost_label(label):
     def count():
         global Cost
         Cost += 0.0002
-        Cost4D = round(Cost, 4)             #convert number to 4 decimal places
-        label.config(text="£" +str(Cost4D))     #display 4 decimal place total preceeded with a pound sign
+        Cost4D = round(Cost, 3)             #convert number to 4 decimal places
+        label.config(text="" +str(Cost4D))     #display 4 decimal place total preceeded with a pound sign
         label.after(1000, count)
 
     count()
@@ -16,9 +16,10 @@ def Cost_label(label):
 root = tk.Tk()
 root.title("The Cost of Speech to Text")
 root.geometry("1500x1000")
-root.configure(bg='blue')
+root.configure(bg='#AB4F98')
+#root.configure(fg='#E73E55')
 
-label = tk.Label(root, fg="yellow", font=('Arial', 140), background="blue",)
+label = tk.Label(root, fg="#E73E55", font=('DSEG7 Classic', 180), background="#AB4F98",)
 label.place(relx = 0.5, rely = 0.5, anchor = CENTER)
 #label.pack()
 Cost_label(label)
